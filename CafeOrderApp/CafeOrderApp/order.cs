99f34023896a5e5463a0b234bdeba1c3cbc2f041
terrafel <terrafel@gmail.com>
2025-05-15 13:21:40 +0700

    punya tera

---
+++ CafeOrderApp/CafeOrderApp/order.cs
@@ -0,0 +1,12 @@
+﻿namespace CafeOrderApp.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public string Item { get; set; }
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
+
